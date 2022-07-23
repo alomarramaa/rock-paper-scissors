e2741d7b2bf0ca1fd3b42b5643f51737f67df5da
alomarramaa <alomar.ramaa@gmail.com>
2022-07-22 21:27:06 -0400

    added functions and bulk code

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 + This game is playable by console.
 + The overall goal of the game is to learn the fundamentals of javascript, and tie them into the skills learned in HTML and CSS.
 + Creative liberty was used in designing this game! The overall structure is the same, but my own artistic elements were used to personalize it.
+ 
--- main.js
+++ main.js
@@ -1,12 +1,76 @@
-function game (){
-    
+const choices = ["rock", "paper", "scissors"];
+const winners = [];
+
+function game() {
+ for (let i = 0; i <= 5; i++){
+  playRound();
+ } 
+ logWins();
 }
 
-function playRound (playerSelection, computerSelection){
+function playRound(round) {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection,computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round)
 
 }
 
-const playerChoice= "rock"
-const comp = "paper"
+function playerChoice() {
+  let input = prompt("Type rock, paper or scissors");
+  while (input == null) {
+    input = prompt("Please try again or check your spelling.");
+  }
 
-playRound(playerChoice, comp)
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt("Please try again or check your spelling.");
+  };
+  while (input == null) {
+    input = prompt("Please try again or check your spelling.");
+  }
+  input = input.toLowerCase();
+  check = validateInput(input);
+  return input;
+}
+
+
+
+
+
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function validateInput(choice) {
+  return choices.includes(choice);
+}
+
+function checkWinner(choiceP, choiceC) {
+  if (choiceP == choiceC) {
+    return "Tie";
+  } else if (
+    (choiceP == "rock" && choiceC == "scissors") ||
+    (choiceP == "paper" && choiceC == "rock") ||
+    (choiceP == "scissors" && choiceC == "paper")
+  ) {
+    return "Player";
+  } else {
+    return "Computer";
+  }
+}
+
+function logWins(){
+console.log(winners);
+}
+
+function logRound(playerChoice, computerChoice, winner, round){
+  console.log('Round: ', round)
+  console.log('Player chose:', playerChoice)
+  console.log('Computer chose:', computerChoice)
+  console.log(winner,'Won the Round')
+}
+
+game();
